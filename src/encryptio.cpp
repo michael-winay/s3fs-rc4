8c99737ed6397c5ae408f6336bc61079d2aaeb89
Michael Winay <mgwinay@gmail.com>
2021-04-18 15:35:05 -0400

    fixed errors in encryption function

--- src/encryptio.cpp
+++ src/encryptio.cpp
@@ -2,11 +2,11 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <iostream>
+#include <string.h>
 
 #include <openssl/rc4.h>
 #include <openssl/evp.h>
 #include <openssl/rand.h>
-#include <openssl/md5.h>
 #include <openssl/err.h>
 
 using namespace std;
@@ -18,20 +18,20 @@ int encryptio (int fd, int enc)
     //encryption variables
     char header[] = "Salted__";
     int inlen, outlen, templen;
+    int keylen = EVP_CIPHER_key_length(EVP_rc4());
     unsigned char saltbuffer[8];
     unsigned char fdbuffer_in[READ_SIZE];
     unsigned char fdbuffer_out[READ_SIZE + EVP_MAX_BLOCK_LENGTH];
 
     //set up encryption key and salt
-    const unsigned char* passphrase = reinterpret_cast<const unsigned char *>("cscfourtyfourtwenty");
-    int passlength = sizeof(passphrase);
-    unsigned char key[EVP_MAX_KEY_LENGTH], iv[EVP_MAX_IV_LENGTH];
+    char passphrase[] = "michael";
+    unsigned char key[keylen];
 
     //generate salt
     if(enc == 1) {
         if(RAND_bytes(saltbuffer, 8) == 0) {
             cout << "Error generating salt" << endl;
-            return 0;
+           return 0;
         }
     }
     //read salt
@@ -48,8 +48,8 @@ int encryptio (int fd, int enc)
 
     //create cipher key
     RC4_KEY rc4key;
-    EVP_BytesToKey(EVP_rc4(), EVP_md5(), saltbuffer, passphrase, passlength, 1, key, iv);
-    RC4_set_key(&rc4key, 16, key);
+    EVP_BytesToKey(EVP_rc4(), EVP_sha256(), saltbuffer, (const unsigned char *)passphrase, strlen(passphrase), 1, key, NULL);
+    RC4_set_key(&rc4key, keylen, key);
 
     //create temp file for storing cipher text, set it to delete after closure of fdcipher
     char cipherTemplate[] = "/tmp/cipherXXXXXX";
@@ -58,12 +58,17 @@ int encryptio (int fd, int enc)
         cout << "Error creating cipher temp file" << endl;
         return 0;
     }
-    //unlink(cipherTemplate);
+    unlink(cipherTemplate);
 
     //write salt to beginning
     if (enc == 1) {
-        pwrite(fdcipher, header, sizeof(header) - 1, 0);
-        pwrite(fdcipher, saltbuffer, sizeof(saltbuffer), sizeof(header) - 1);
+        inlen = pwrite(fdcipher, header, sizeof(header) - 1, 0);
+        inlen = pwrite(fdcipher, saltbuffer, sizeof(saltbuffer), sizeof(header) - 1);
+
+        if (inlen == -1) {
+            cout << "Error writing salt to file" << endl;
+            return 0;
+        }
     }
 
     //encrypt/ decrypt file, write encrypted text to temp file
@@ -74,7 +79,6 @@ int encryptio (int fd, int enc)
         if(enc == 0) inlen = pread(fd, fdbuffer_in, READ_SIZE, (i* READ_SIZE) + ((sizeof(header) - 1) + sizeof(saltbuffer)));
 
         if (inlen == -1) {
-            /* Error */
             cout << "Error reading file for encryption" << endl;
             return 0;
         }
@@ -85,7 +89,6 @@ int encryptio (int fd, int enc)
         if(enc == 1) templen = pwrite(fdcipher, fdbuffer_out, inlen, (i* READ_SIZE) + ((sizeof(header) - 1) + sizeof(saltbuffer)));
         if(enc == 0) templen = pwrite(fdcipher, fdbuffer_out, inlen, i * READ_SIZE);
         if(templen == -1) {
-            /* Error */
             cout << "Error writing to file from encryption" << endl;
             return 0;
         }
@@ -110,10 +113,10 @@ int encryptio (int fd, int enc)
             close(fdcipher);
             return 0;
         }
-        if (inlen < READ_SIZE) break;
+        if (inlen < READ_SIZE) {
+            break;
+        }
     }
-
     //free cipher memory
-    close(fdcipher);
     return 1;
 }
